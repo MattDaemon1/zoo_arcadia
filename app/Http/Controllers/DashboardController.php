0963ca9893f37d0659845ae7bfc0e06fd3713096
MattDaemon1 <matt@mattkonnect.com>
2024-08-18 17:03:32 +0200

    dashboard veto et employe ok

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,19 +4,49 @@ namespace App\Http\Controllers;
 
 use App\Models\Animal;
 use App\Models\Visite;
+use App\Models\Habitat;
+use App\Models\Consommation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     /**
-     * Show the admin dashboard.
+     * Show the dashboard.
      *
      * @return \Illuminate\View\View
      */
     public function index()
     {
-        $animals = Animal::all(); // Pour le filtre par animal
+        $user = Auth::user();
 
-        return view('dashboard.index', compact('animals'));
+        // Récupération des données pour les vétérinaires, les administrateurs et les employés
+        $animals = Animal::all();
+        $visites = null;
+        $habitats = null;
+        $consommations = null;
+
+        // Administrateur
+        if ($user->role->label === 'admin') {
+            $habitats = Habitat::all();
+            $visites = Visite::all();
+            $consommations = Consommation::all();
+        }
+
+        // Vétérinaire
+        if ($user->role->label === 'veterinaire') {
+            $visites = Visite::where('veterinaire_id', $user->id)->get();
+            $habitats = Habitat::all();
+            $consommations = Consommation::all();
+        }
+
+        // Employé
+        if ($user->role->label === 'employe') {
+            $consommations = Consommation::where('employe_id', $user->id)->get();
+            $habitats = Habitat::all();
+        }
+
+        // On passe les données à la vue
+        return view('dashboard.index', compact('user', 'animals', 'visites', 'habitats', 'consommations'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,75 +4,160 @@
     {{ __('Admin Dashboard') }}
 @endsection
 
+
 @section('content')
     <div class="container">
-        <h1>{{ __('Admin Dashboard') }}</h1>
+        <h1>Dashboard</h1>
 
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card mb-4">
-                    <div class="card-header">{{ __('Manage Users') }}</div>
-                    <div class="card-body">
-                        <a href="{{ route('users.index') }}" class="btn btn-primary">{{ __('View Users') }}</a>
-                        <a href="{{ route('users.create') }}" class="btn btn-success">{{ __('Create New User') }}</a>
+        <!-- Section commune pour tous les rôles -->
+        <div>
+            <h2>Informations Générales</h2>
+            <h1>{{ Auth::user()->role->name }}</h1>
+
+            <!-- Insérer ici des informations accessibles à tous -->
+        </div>
+
+        <!-- Section Admin -->
+        @if(Auth::user()->role->label === 'admin')
+            <div>
+                <h2>Section admin</h2>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Manage Users') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('users.index') }}" class="btn btn-primary">{{ __('View Users') }}</a>
+                                <a href="{{ route('users.create') }}" class="btn btn-success">{{ __('Create New User') }}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Manage Animals') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('animals.index') }}" class="btn btn-primary">{{ __('View Animals') }}</a>
+                                <a href="{{ route('animals.create') }}" class="btn btn-success">{{ __('Create New Animal') }}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Manage Habitats') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('habitats.index') }}" class="btn btn-primary">{{ __('View Habitats') }}</a>
+                                <a href="{{ route('habitats.create') }}" class="btn btn-success">{{ __('Create New Habitat') }}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Manage Races') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('races.index') }}" class="btn btn-primary">{{ __('View Races') }}</a>
+                                <a href="{{ route('races.create') }}" class="btn btn-success">{{ __('Create New Races') }}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Manage Services') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('services.index') }}" class="btn btn-primary">{{ __('View Services') }}</a>
+                                <a href="{{ route('services.create') }}" class="btn btn-success">{{ __('Create New Service') }}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Veterinary Reports') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('visites.index') }}" class="btn btn-primary">{{ __('View Reports') }}</a>
+                                <form method="GET" action="{{ route('visites.index') }}" class="mt-3">
+                                    <div class="form-group">
+                                        <label for="animal">{{ __('Filter by Animal') }}</label>
+                                        <select id="animal" name="animal_id" class="form-control">
+                                            <option value="">{{ __('Select Animal') }}</option>
+                                            @foreach($animals as $animal)
+                                                <option value="{{ $animal->id }}">{{ $animal->prenom }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group mt-2">
+                                        <label for="date">{{ __('Filter by Date') }}</label>
+                                        <input type="date" id="date" name="date" class="form-control">
+                                    </div>
+                                    <button type="submit" class="btn btn-secondary mt-3">{{ __('Apply Filters') }}</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+        @endif
 
-            <div class="col-md-6">
-                <div class="card mb-4">
-                    <div class="card-header">{{ __('Manage Animals') }}</div>
-                    <div class="card-body">
-                        <a href="{{ route('animals.index') }}" class="btn btn-primary">{{ __('View Animals') }}</a>
-                        <a href="{{ route('animals.create') }}" class="btn btn-success">{{ __('Create New Animal') }}</a>
+        <!-- Section Vétérinaire -->
+        @if($user->role->label === 'veterinaire')
+            <div>
+                <h2>Veterinarian Section</h2>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Veterinary Reports') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('visites.create') }}" class="btn btn-success">{{ __('Add New Report') }}</a>
+                                <a href="{{ route('visites.index') }}" class="btn btn-primary">{{ __('View All Reports') }}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Manage Habitat Reviews') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('habitats.index') }}" class="btn btn-primary">{{ __('View Habitats') }}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="card mb-4">
+                            <div class="card-header">{{ __('Animal Consumption') }}</div>
+                            <div class="card-body">
+                                <a href="{{ route('consommations.index') }}" class="btn btn-primary">{{ __('Voir les consommations') }}</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+        @endif
 
-            <div class="col-md-6">
-                <div class="card mb-4">
-                    <div class="card-header">{{ __('Manage Habitats') }}</div>
-                    <div class="card-body">
-                        <a href="{{ route('habitats.index') }}" class="btn btn-primary">{{ __('View Habitats') }}</a>
-                        <a href="{{ route('habitats.create') }}" class="btn btn-success">{{ __('Create New Habitat') }}</a>
-                    </div>
-                </div>
+        <!-- Section Employé -->
+        @if($user->role->label === 'employe')
+            <div>
+                <h2>Employé Section</h2>
+                <!-- Ajouter ici les éléments spécifiques à l'employé -->
+                <p>Consommations que vous avez enregistrées :</p>
+                <ul>
+                    @foreach($consommations as $consommation)
+                        <li>{{ $consommation->date }} - {{ $consommation->animal->prenom }} - {{ $consommation->nourriture }}: {{ $consommation->quantite }}g</li>
+                    @endforeach
+                </ul>
             </div>
-
-            <div class="col-md-6">
-                <div class="card mb-4">
-                    <div class="card-header">{{ __('Manage Services') }}</div>
-                    <div class="card-body">
-                        <a href="{{ route('services.index') }}" class="btn btn-primary">{{ __('View Services') }}</a>
-                        <a href="{{ route('services.create') }}" class="btn btn-success">{{ __('Create New Service') }}</a>
-                    </div>
-                </div>
-            </div>
-
             <div class="col-md-12">
                 <div class="card mb-4">
-                    <div class="card-header">{{ __('Veterinary Reports') }}</div>
+                    <div class="card-header">{{ __('Animal Consumption') }}</div>
                     <div class="card-body">
-                        <a href="{{ route('visites.index') }}" class="btn btn-primary">{{ __('View Reports') }}</a>
-                        <form method="GET" action="{{ route('visites.index') }}" class="mt-3">
-                            <div class="form-group">
-                                <label for="animal">{{ __('Filter by Animal') }}</label>
-                                <select id="animal" name="animal_id" class="form-control">
-                                    <option value="">{{ __('Select Animal') }}</option>
-                                    @foreach($animals as $animal)
-                                        <option value="{{ $animal->id }}">{{ $animal->prenom }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group mt-2">
-                                <label for="date">{{ __('Filter by Date') }}</label>
-                                <input type="date" id="date" name="date" class="form-control">
-                            </div>
-                            <button type="submit" class="btn btn-secondary mt-3">{{ __('Apply Filters') }}</button>
-                        </form>
+                        <a href="{{ route('consommations.create') }}" class="btn btn-primary">{{ __('Créer une consommation') }}</a>
+                        <a href="{{ route('consommations.index') }}" class="btn btn-primary">{{ __('View Animal Consumption') }}</a>
                     </div>
-                </div>
-            </div>
-        </div>
+                    </div>
+                    </div>
+        @endif
     </div>
 @endsection
